570ac9056b6e2e68b2fcc66470c02664b920389c
z2s <zen2see@gmail.com>
2019-07-05 18:05:10 -0400

    Work up until buyItem

--- contracts/SupplyChain.sol
+++ contracts/SupplyChain.sol
@@ -17,7 +17,8 @@ contract SupplyChain {
   /* Add a line that creates a public mapping that maps the SKU (a number) to an Item.
      Call this mappings items
   */
-  mapping (address ==> uint) public items;
+  mapping (uint ==> Item) public items;
+
   /* Add a line that creates an enum called State. This should have 4 states
     ForSale
     Sold
@@ -37,20 +38,37 @@ contract SupplyChain {
   */
   struct Item {
     string name;
-    uint sku; 
+    uint sku;
     uint price;
-    bool states;
+    State state;
     address payable seller;
     address payable buyer;
-
   }
 
   /* Create 4 events with the same name as each possible State (see above)
     Prefix each event with "Log" for clarity, so the forSale event will be called "LogForSale"
     Each event should accept one argument, the sku */
+  event LogForSale(
+    uint sku
+    );
+  event LogSold(
+    uint sku
+    );
+  event LogShipped(
+    uint sku
+    );
+  event LogReceived(
+    uint sku
+    );
 
-/* Create a modifer that checks if the msg.sender is the owner of the contract */
-
+  /* Create a modifer that checks if the msg.sender is the owner of the contract */
+  modifier isOwner {
+    require(
+      msg.sender == owner,
+      "Only owner can call this function."
+      );
+      _;
+  }
   modifier verifyCaller (address _address) { require (msg.sender == _address); _;}
 
   modifier paidEnough(uint _price) { require(msg.value >= _price); _;}
@@ -69,15 +87,16 @@ contract SupplyChain {
    so checking that Item.State == ForSale is not sufficient to check that an Item is for sale.
    Hint: What item properties will be non-zero when an Item has been added?
    */
-  modifier forSale
-  modifier sold
-  modifier shipped
-  modifier received
-
+  modifier forSale(uint _sku) { require (uint(items[_sku].State) == State.ForSale); _;}
+  modifier sold(uint _sku) { require (uint(items[_sku].State) == State.Sold); _;}
+  modifier shipped(uint _sku) { require (uint(items[_sku].State) == State.Shipped); _;}
+  modifier received(uint _sku) { require (uint(itmes[_sku].State) == State.Received); _;}
 
   constructor() public {
     /* Here, set the owner as the person who instantiated the contract
        and set your skuCount to 0. */
+       owner = msg.sender;
+       skuCount = 0;
   }
 
   function addItem(string memory _name, uint _price) public returns(bool){
@@ -92,10 +111,11 @@ contract SupplyChain {
     to Sold. Be careful, this function should use 3 modifiers to check if the item is for sale,
     if the buyer paid enough, and check the value after the function is called to make sure the buyer is
     refunded any excess ether sent. Remember to call the event associated with this function!*/
+  function buyItem(uint sku) public payable forSale(sku), paidEnough(sku), checkValue(sku) {
+    
 
-  function buyItem(uint sku)
-    public
-  {}
+      )
+  }
 
   /* Add 2 modifiers to check if the item is sold already, and that the person calling this function
   is the seller. Change the state of the item to shipped. Remember to call the event associated with this function!*/
